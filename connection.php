dae8d2074feb353b467833bf77ca6d025f1415e4
Joshua Prince <107548448+JoshuaPrinceGeorge@users.noreply.github.com>
2022-07-06 04:12:04 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $db_host = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "github-bruteforce";
+$db_name = "github-sqlinjection";
 
 $connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
